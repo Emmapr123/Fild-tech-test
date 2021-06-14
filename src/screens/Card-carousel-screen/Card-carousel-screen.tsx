4dc7c9af6308660556d20253aea6060765811323
Emmapr123 <emmapriester@hotmail.com>
2021-06-14 10:14:41 +0100

    Added card component

--- src/screens/Card-carousel-screen/Card-carousel-screen.tsx
+++ src/screens/Card-carousel-screen/Card-carousel-screen.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, 
+  SafeAreaView, 
+  ScrollView, 
+  Text, 
+  View, 
+  Dimensions,
+  StyleSheet } from 'react-native'
+import { CardComponent } from '../../components'
 
 const CardCarouselScreen = () => {
 
@@ -7,33 +14,46 @@ const CardCarouselScreen = () => {
     id: 1,
     location: 'United Kingdom',
     title: 'Virtually Hosted Games - The Night at The Opera House',
-    price: 25.50
+    price: '25.50'
   },{
     id: 2,
     location: 'United Kingdom',
     title: 'Virtually Hosted Games - Carnaval Night',
-    price: 40.50
+    price: '40.50'
   },{
     id: 3,
     location: 'Kenya',
     title: 'Live Relaxing Traditional African Meditation',
-    price: 10.00 
+    price: '10.00'
   },{
     id: 4,
     location: 'India',
     title: 'Singing Ventriloquism with Yoko',
-    price: 45.00
+    price: '45.00'
   },{
     id: 5,
     location: '',
     title: 'Traditional African Cooking with Gillian',
-    price: 30.00
+    price: '30.00'
   }]
-  
+
+  const { width, height } = Dimensions.get('window')
+
   return(
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <Text> Online Experiences </Text>
+      <FlatList 
+      data={data}
+      horizontal
+      keyExtractor={(data) => data.title}
+      bounces={false}
+      ItemSeparatorComponent={() => <View style={{width: width / 10}}/>}
+      snapToInterval={width}
+      renderItem={({item, index}) => {
+        return <CardComponent key={index} card={item} />
+      }}
+      />
+    </SafeAreaView>
   )
 }
 
