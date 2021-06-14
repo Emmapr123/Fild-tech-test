90e8c907def8600570554d88066c4b3bf23be528
Emmapr123 <emmapriester@hotmail.com>
2021-06-14 11:22:02 +0100

    Created a functional like button and added a gradient to make a button vissible on darker images

--- src/SVGs/LikeButton/LikeButton.tsx
+++ src/SVGs/LikeButton/LikeButton.tsx
@@ -3,11 +3,9 @@ import Svg, { SvgProps, Path } from "react-native-svg"
 
 function LikeButton(props: SvgProps) {
 
-  const { width=40, height=148.8, color="#1d1d1b" } = props
-  
+  const { width=42, height=148.8, color="#1d1d1b", fill="none" } = props
   return (
     <Svg
-      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 113.95 111.6"
@@ -15,9 +13,10 @@ function LikeButton(props: SvgProps) {
     >
       <Path
         d="M57.18 26.92C63.95 4.16 76.44-.34 89.23 1.11c16.55 1.87 24.22 11.2 24.23 47.54 0 40.33-49.41 62.45-56 62.45C52.02 111.1.68 88.83.52 48.65.38 11.71 11.91.52 26.26.5 36.97.49 51.48 5.96 57.18 26.92z"
-        fill={color}
+        fill={fill}
         stroke={color}
-        strokeMiterlimit={10}
+        strokeWidth={5}
+        strokeMiterlimit={40}
       />
     </Svg>
   )
--- src/components/Card-Component/Card-component.tsx
+++ src/components/Card-Component/Card-component.tsx
@@ -5,6 +5,8 @@ import {
     Dimensions,
     Image
   } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import { LikeButton } from '../../SVGs';
 import { Button } from '../Button';
 
 interface Card {
@@ -24,13 +26,20 @@ const CardComponent = ( {card}: CardComponentProps) => {
   const { width, height } = Dimensions.get('window')
   const [liked, setLiked] = useState(false)
 
+  console.log(liked)
+
   return(
     <View style={{borderRadius: 18, width: width * 0.9, backgroundColor: 'lightgray', flex: 1}}>
-      <Image 
-      style={{width: width * 0.9, height: height / 3 * 2, borderTopLeftRadius: 18, borderTopRightRadius: 18}}
-      source={{uri: card.image}}
-      /> 
-      
+      <View style={{position: 'relative'}}>
+         <Image 
+        style={{width: width * 0.9, height: height / 3 * 2, borderTopLeftRadius: 18, borderTopRightRadius: 18}}
+        source={{uri: card.image}}
+        /> 
+        <LinearGradient colors={['transparent', 'lightgray']} style={{position: 'absolute', left: 0, bottom: 0, right: 0}}>
+          <View style={{height:  100}}/>
+        </LinearGradient>       
+      </View>
+      <Button style={{position: 'absolute', top: (height / 3 * 2 ) - 100, right: 10}} text={<LikeButton fill={liked ? 'black' : 'none'}/>} onPress={() => liked ? setLiked(false) : setLiked(true)}/>
       <View style={{padding: 10}}>
         <Text>{card.location}</Text>
         <Text style={{fontWeight: 'bold', fontSize: 24}}>{card.title}</Text>
