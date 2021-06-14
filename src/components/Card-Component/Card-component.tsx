b57febe5e00b863a12915b809783c4b18e0e7156
Emmapr123 <emmapriester@hotmail.com>
2021-06-14 10:31:43 +0100

    Added images

--- src/components/Card-Component/Card-component.tsx
+++ src/components/Card-Component/Card-component.tsx
@@ -2,26 +2,33 @@ import React from 'react';
 import {
     View,
     Text,
-    Dimensions
+    Dimensions,
+    Image
   } from 'react-native'
 
 interface Card {
   id: number,
+  image: string,
   location: string,
   title: string,
   price: string
 }
 
 interface CardComponentProps {
-  key: number,
   card: Card
 }
 
-const CardComponent = ( {key, card}: CardComponentProps) => {
+const CardComponent = ( {card}: CardComponentProps) => {
+
+  // console.log(card)
 
   const { width, height } = Dimensions.get('window')
   return(
     <View style={{borderWidth: 1, borderColor: 'black', borderRadius: 18, width: width * 0.8}}>
+      <Image 
+      style={{width: width * 0.8, height: height / 3 * 2}}
+      source={{uri: card.image}}
+      /> 
       <Text>{card.location}</Text>
       <Text>{card.title}</Text>
       <Text>{card.price} GBP</Text>
--- src/screens/Card-carousel-screen/Card-carousel-screen.tsx
+++ src/screens/Card-carousel-screen/Card-carousel-screen.tsx
@@ -12,27 +12,32 @@ const CardCarouselScreen = () => {
 
   const data = [{
     id: 1,
+    image: 'https://images.unsplash.com/flagged/photo-1570286424640-675cdc1cbd5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
     location: 'United Kingdom',
     title: 'Virtually Hosted Games - The Night at The Opera House',
     price: '25.50'
   },{
     id: 2,
+    image: 'https://images.unsplash.com/photo-1574724713425-fee7e2eacf84?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1312&q=80',
     location: 'United Kingdom',
     title: 'Virtually Hosted Games - Carnaval Night',
     price: '40.50'
   },{
-    id: 3,
+    id: 3,    
+    image: 'https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=862&q=80',
     location: 'Kenya',
     title: 'Live Relaxing Traditional African Meditation',
     price: '10.00'
   },{
     id: 4,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzvHoq_ttbDnkaDNJcpUOkI1R9hkXyfyGYbeeTAh9b4Q_g4w0cuKjTod9YzigMtJp08Hg&usqp=CAU',
     location: 'India',
     title: 'Singing Ventriloquism with Yoko',
     price: '45.00'
   },{
     id: 5,
-    location: '',
+    image: 'https://images.unsplash.com/photo-1611712142269-12b7433e28e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    location: 'Africa',
     title: 'Traditional African Cooking with Gillian',
     price: '30.00'
   }]
