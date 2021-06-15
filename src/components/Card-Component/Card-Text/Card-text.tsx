021c8a3a5ed26ee2e4d98b2f2b178c4484596d31
Emmapr123 <emmapriester@hotmail.com>
2021-06-15 09:43:41 +0100

    The text now has their own positions

--- src/components/Card-Component/Card-Text/Card-text.tsx
+++ src/components/Card-Component/Card-Text/Card-text.tsx
@@ -8,12 +8,18 @@ import { CardComponentProps } from '../Card-component.types';
 
 const CardText = ( {card}:CardComponentProps) => {
   return(
-    <View style={{padding: 10}}>
-    <Text>{card.location}</Text>
-    <Text style={styles.boldText}>{card.title}</Text>
-    <Text>Starting from</Text>
-    <Text style={styles.boldText}>{card.price} GBP</Text>
-  </View>
+    <View style={{flex: 1, position: 'relative'}}>
+      <View style={{padding: 10}}>
+        <Text>{card.location}</Text>
+      </View>
+      <View style={{position: 'absolute', top: 30, left: 10, right: 10}}>
+        <Text style={styles.boldText}>{card.title}</Text>
+      </View>
+      <View style={{position: 'absolute', bottom: 10, left: 10}}>
+        <Text>Starting from</Text>
+        <Text style={styles.boldText}>{card.price} GBP</Text>
+      </View>
+    </View>
   )
 }
 
